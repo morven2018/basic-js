b5c6d47e882555e5011f8c7d958d7c4309ad95ac
morven2018 <you@example.com>
2025-01-01 22:55:22 +0300

    feat: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  const controlSequences = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  const newArr = [...arr];
+  newArr.forEach((item, index) => {
+    if (item === controlSequences[0]) markElement(newArr, index, false);
+    if (item === controlSequences[1]) markElement(newArr, index);
+    if (item === controlSequences[2]) doubleElement(newArr, index, false);
+    if (item === controlSequences[3]) doubleElement(newArr, index);
+  }
+  )
+  return newArr.filter(item => item !== 'to_del');
 }
+
+function markElement(arr, index, prev = true) {
+  if (prev && index !== 0) {
+    arr[index - 1] = 'to_del';
+  } else if (!prev && index + 1 < arr.length) {
+    arr[index + 1] = 'to_del';
+  }
+  arr[index] = 'to_del';
+}
+
+function doubleElement(arr, index, prev = true) {
+  if (prev && index !== 0) {
+    arr[index] = arr[index - 1]
+  } else if (!prev && index + 1 < arr.length) {
+    arr[index] = arr[index + 1];
+  } else {
+    arr[index] = 'to_del';
+  }
+}
+
+
 
 module.exports = {
   transform
