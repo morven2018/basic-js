681bb7ed86dbd579773d1e5dde22f7a8d5d08fe4
morven2018 <you@example.com>
2025-01-02 21:23:36 +0300

    feat: add cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor (direct = true){
+    this.direct = direct;
+    this.alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    this.matrix = Array(this.alphas.length).fill([]);
+    for (let i = 0; i < this.alphas.length; i += 1){
+      this.matrix[i] = Array(this.alphas.length).fill('');
+      for (let j = 0; j < this.alphas.length; j += 1){
+        this.matrix[i][j] = this.alphas[(i + j) % this.alphas.length];
+      }
+    }
+
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  encrypt(message, key) {
+    try{
+      if(message === undefined || key === undefined) throw new Error("Incorrect arguments!");
+      
+      const toEncrypt = message.toUpperCase().split("");
+      const result = [];
+      const upperKey = key.toUpperCase();
+      let garbage = 0;
+      toEncrypt.forEach((item, index) => {
+        if (this.alphas.includes(item)){
+          let i = this.alphas.indexOf(item);
+          let j = this.alphas.indexOf(upperKey[(index - garbage) % key.length]);
+          result.push(this.matrix[i][j]);
+          return this.matrix[i][j];
+        }else{
+            result.push(item);
+            garbage += 1;
+        } 
+      });
+      if (this.direct) return result.join('');
+      else return result.reverse().join('');
+    }
+    catch (err){
+      throw new Error("Incorrect arguments!");
+    }
+  }
+
+  
+  decrypt(message, key) {
+    try{
+      if(message === undefined || key === undefined) throw new Error("Incorrect arguments!");
+      
+      const toEncrypt = message.toUpperCase().split("");
+      const result = [];
+      const upperKey = key.toUpperCase();
+      let garbage = 0;
+      toEncrypt.forEach((item, index) => {
+        if (this.alphas.includes(item)){
+          let i = this.alphas.indexOf(upperKey[(index - garbage) % key.length]);
+          let j = this.matrix[i].indexOf(item);
+          result.push(this.alphas[j]);
+          return this.alphas[j];
+        }else{
+            result.push(item);
+            garbage += 1;
+        } 
+      });
+      if (this.direct) return result.join('');
+      else return result.reverse().join('');
+    }
+    catch (err){
+      throw new Error("Incorrect arguments!");
+    }
   }
 }
 
